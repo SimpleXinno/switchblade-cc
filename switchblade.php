2b9b59c17b5e8c23ad36b2f391b28177143afab7
SimpleXinno <surajperson767@gmail.com>
2026-07-29 08:02:50 +0530

    Update switchblade.php

--- switchblade.php
+++ switchblade.php
@@ -6,7 +6,7 @@
 // Webhook: https://api.telegram.org/bot<token>/setwebhook?url=<url>
 
 // BOT API Configuration
-$botToken = "TOKEN"; #<------------------- PUT YOUR TOKEN HERE------------->#
+$botToken = "8817181082:AAHGNq4YfcNv0OzCOe9VomF9ZJ3-jpEFEKI"; #<------------------- PUT YOUR TOKEN HERE------------->#
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
